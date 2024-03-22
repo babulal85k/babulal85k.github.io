9de0e737c38d8d608dd6f3f8a93daddd189b269c
babulal85k <madhukarbabulal5@gmail.com>
2024-03-23 00:37:46 +0530

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,183 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div id='footer'>
-      
-    </div>
-  )
-}
+    <div id="footer">
+      <p className="field">
+        <a
+          className="button is-medium"
+          href="https://twitter.com/yourcoderishere"
+          target="_blank"
+        >
+          <span className="icon is-small">
+            <svg
+              className="svg-inline--fa fa-twitter fa-fw"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="twitter"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+              />
+            </svg>
+            {/* <i class="fab fa-twitter fa-fw"></i> */}
+          </span>
+        </a>
+        <a
+          className="button is-medium"
+          href="https://dribbble.com/babulal85k"
+          target="_blank"
+        >
+          <span className="icon is-small">
+            <svg
+              className="svg-inline--fa fa-dribbble fa-fw"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="dribbble"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M256 8C119.252 8 8 119.252 8 256s111.252 248 248 248 248-111.252 248-248S392.748 8 256 8zm163.97 114.366c29.503 36.046 47.369 81.957 47.835 131.955-6.984-1.477-77.018-15.682-147.502-6.818-5.752-14.041-11.181-26.393-18.617-41.614 78.321-31.977 113.818-77.482 118.284-83.523zM396.421 97.87c-3.81 5.427-35.697 48.286-111.021 76.519-34.712-63.776-73.185-116.168-79.04-124.008 67.176-16.193 137.966 1.27 190.061 47.489zm-230.48-33.25c5.585 7.659 43.438 60.116 78.537 122.509-99.087 26.313-186.36 25.934-195.834 25.809C62.38 147.205 106.678 92.573 165.941 64.62zM44.17 256.323c0-2.166.043-4.322.108-6.473 9.268.19 111.92 1.513 217.706-30.146 6.064 11.868 11.857 23.915 17.174 35.949-76.599 21.575-146.194 83.527-180.531 142.306C64.794 360.405 44.17 310.73 44.17 256.323zm81.807 167.113c22.127-45.233 82.178-103.622 167.579-132.756 29.74 77.283 42.039 142.053 45.189 160.638-68.112 29.013-150.015 21.053-212.768-27.882zm248.38 8.489c-2.171-12.886-13.446-74.897-41.152-151.033 66.38-10.626 124.7 6.768 131.947 9.055-9.442 58.941-43.273 109.844-90.795 141.978z"
+              />
+            </svg>
+            {/* <i class="fab fa-dribbble fa-fw"></i> */}
+          </span>
+        </a>
+        <a
+          className="button is-medium"
+          href="https://linkedin.com/in/babulal85k/"
+          target="_blank"
+        >
+          <span className="icon is-small">
+            <svg
+              className="svg-inline--fa fa-linkedin-in fa-fw"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="linkedin-in"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+              />
+            </svg>
+            {/* <i class="fab fa-linkedin-in fa-fw"></i> */}
+          </span>
+        </a>
+        <a
+          className="button is-medium"
+          href="https://www.indiehackers.com/babulal85k"
+          target="_blank"
+        >
+          <span className="icon is-small">
+            <svg
+              className="svg-inline--fa fa-indie-hackers fa-fw"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fak"
+              data-icon="indie-hackers"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M180 80H96V416H180V80zM348 80H264V416H348V290H460V416H544V80H460V206H348V80z"
+              />
+            </svg>
+            {/* <i class="fak fa-indie-hackers fa-fw"></i> */}
+          </span>
+        </a>
+        <a
+          className="button is-medium"
+          href="https://www.producthunt.com/@babu_lal_mandal"
+          target="_blank"
+        >
+          <span className="icon is-small">
+            <svg
+              className="svg-inline--fa fa-product-hunt fa-fw"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="product-hunt"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M326.3 218.8c0 20.5-16.7 37.2-37.2 37.2h-70.3v-74.4h70.3c20.5 0 37.2 16.7 37.2 37.2zM504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zm-128.1-37.2c0-47.9-38.9-86.8-86.8-86.8H169.2v248h49.6v-74.4h70.3c47.9 0 86.8-38.9 86.8-86.8z"
+              />
+            </svg>
+            {/* <i class="fab fa-product-hunt fa-fw"></i> */}
+          </span>
+        </a>
+        <a className="button is-medium" href="mailto:madhukarbabulal5@gmail.com">
+          <span className="icon is-small">
+            <svg
+              className="svg-inline--fa fa-envelope fa-fw"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="envelope"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"
+              />
+            </svg>
+            {/* <i class="far fa-envelope fa-fw"></i> */}
+          </span>
+        </a>
+      </p>
 
-export default Footer
+      <div className="copyright">
+        Handcrafted by me
+        <span className="icon has-white-text" style={{ verticalAlign: "middle" }}>
+          <svg
+            className="svg-inline--fa fa-copyright"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="far"
+            data-icon="copyright"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            data-fa-i2svg=""
+            style={{ width: "1em", height: "1em" }} // Adjust size if necessary
+          >
+            <path
+              fill="currentColor"
+              d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM199.4 312.6c-31.2-31.2-31.2-81.9 0-113.1s81.9-31.2 113.1 0c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9c-50-50-131-50-181 0s-50 131 0 181s131 50 181 0c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0c-31.2 31.2-81.9 31.2-113.1 0z"
+            ></path>
+          </svg>
+        </span>
+        Babu Lal Mandal
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
