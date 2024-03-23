66d42dc946453bfd8b655f81d6362e10bfcec8cc
babulal85k <madhukarbabulal5@gmail.com>
2024-03-23 07:16:41 +0530

    css update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import "./contact.css";
 
 const Contact = () => {
 
@@ -18,46 +19,48 @@ const Contact = () => {
 
   return (
     <div id='contact'>
-      <div className="contact-form">
-        <h2>Contact Me</h2>
-        <form action="https://formspree.io/f/mvoegwnv" method="post" id="contactForm">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Enter  Your Name..."
-            name="name"
-            required=""
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter Your Email Adress..."
-            name="email"
-            required=""
-          />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            placeholder="Your massage"
-            rows={4}
-            required=""
-            defaultValue={""}
-          />
-          <button type="submit" id="sub">
-            Send
-          </button>
-        </form>
-      </div>
-      <div className="socialMedia">
-          <button className="whatsapp" onClick={() => handleClick('whatsapp')}>WhatsApp
-            <p>+917488230355</p>
-          </button>
-          <button className="gmail" onClick={() => handleClick('gmail')}>Gmail
-            <p>madhukarbabulal5@gmail.com</p>
-          </button>
+      <h2>Contact Me</h2>
+      <div className="cont-container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/mvoegwnv" method="post" id="contactForm">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Enter  Your Name..."
+              name="name"
+              required=""
+            />
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter Your Email Adress..."
+              name="email"
+              required=""
+            />
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Your massage"
+              rows={4}
+              required=""
+              defaultValue={""}
+            />
+            <button type="submit" id="sub">
+              Send
+            </button>
+          </form>
+        </div>
+        <div className="socialMedia">
+            <button className="whatsapp" onClick={() => handleClick('whatsapp')}>WhatsApp
+              <p>+917488230355</p>
+            </button>
+            <button className="gmail" onClick={() => handleClick('gmail')}>Gmail
+              <p>madhukarbabulal5@gmail.com</p>
+            </button>
+          </div>
         </div>
     </div>
   )
